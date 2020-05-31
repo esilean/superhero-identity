95d9df93b7cf95c27c403eca58751b246fb8805a
esilean <le.bevilaqua@gmail.com>
2020-05-31 17:04:02 -0300

    commit all files

---
+++ Application/Interfaces/IJwtGenerator.cs
@@ -0,0 +1,10 @@
+
+using Data.Models;
+
+namespace Application.Interfaces
+{
+    public interface IJwtGenerator
+    {
+        string CreateToken(AppUser user);
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
